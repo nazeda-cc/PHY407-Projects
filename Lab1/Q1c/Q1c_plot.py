8fce2bf530c75cb10f45cd0923ee75add9b11af8
nazeda-cc <71141331+nazeda-cc@users.noreply.github.com>
2020-09-13 21:50:43 -0400

    Finish Q1 b and c

---
+++ Lab1/Q1c/Q1c_plot.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 13 19:16:47 2020
+
+@author: rundo
+"""
+import scipy.constants as spc
+import numpy as np
+from matplotlib import pyplot as plt
+from Q1c_function import Euler_Cromer as ec
+
+#Take initial values, and convert into SI unit
+x_0 = 0.47 * spc.au
+y_0 = 0
+vx_0 = 0
+vy_0 = 8.17 * spc.au / 31622400
+dt = 31622400 * 0.0001
+t = 31622400
+
+#Simulation
+result  = ec(x_0, y_0, vx_0, vy_0, dt, t)
+
+#Convert back into AU & year
+x = [i / spc.au for i in result[0]]
+y = [i / spc.au for i in result[1]]
+vx = [i * 31622400 / spc.au for i in result[2]]
+vy = [i * 31622400 / spc.au for i in result[3]]
+
+x_f = x[int(t/dt)-1]
+y_f = y[int(t/dt)-1]
+vx_f = vx[int(t/dt)-1]
+vy_f = vy[int(t/dt)-1]
+print(x_f, y_f, vx_f, vy_f)
+
+
+'''
+#Plot the orbit
+plt.plot(x,y)
+plt.scatter(0,0,color='red')
+plt.text(0.02,0,'Sun')
+plt.xlabel('$x$ (AU)')
+plt.ylabel('$y$ (AU)')
+plt.title('Orbit, $x$ vs $y$')
+'''
+
+'''
+#Plot vx vs x
+plt.plot(x,vx)
+plt.xlabel('$x$ (AU)')
+plt.ylabel('$v_x$ (AU/yr)')
+plt.title('$v_x$ vs $x$')
+'''
+'''
+#Plot vy vs y
+plt.plot(y,vy)
+plt.xlabel('$y$ (AU)')
+plt.ylabel('$v_y$ (AU/yr)')
+plt.title('$v_y$ vs $y$')
+'''
--- Lab1/Q1c_function.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 13 18:45:59 2020
-
-@author: rundo
-"""
